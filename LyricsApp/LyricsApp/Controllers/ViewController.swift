b977971aaa891d4bb53fe7b94e9ff41e45bbc64d
vlari <obed1355@gmail.com>
2021-11-12 05:26:17 -0400

    add project

---
+++ LyricsApp/LyricsApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LyricsApp
+//
+//  Created by Obed Garcia on 8/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
